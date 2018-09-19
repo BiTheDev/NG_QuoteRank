2b0129f89160a0213bd41aadb33beeb158f13128
BrianHo <Curry30@Brians-MacBook-Pro.local>
2018-09-19 14:57:14 -0700

    cleaning code

--- server/model.js
+++ server/model.js
@@ -3,19 +3,6 @@ const goose = require("mongoose");
 
 goose.connect("mongodb://localhost:27017/QuoteRank", {useNewUrlParser: true},(errs)=> console.log(errs?errs:"db QuoteRank"));
 
-
-const QuoteSchema = new goose.Schema({
-    title:{
-        type:String,
-        minlength:[5, "Your Quote is too short"]
-    },
-    Vote:{
-        type:Number,
-        default: 0
-    }
-},{timestamps : true})
-
-
 const AuthorSchema = new goose.Schema({
     name:{
         type :String,
